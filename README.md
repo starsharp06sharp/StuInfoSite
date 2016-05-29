37dbc74e03150c57cac2464f2e041f3f0a960cf5
starsharp06sharp <zhenglei@std.uestc.edu.cn>
2016-05-29 18:29:37 +0800

    修正create_table

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ pip3 install PyMySQL Flask
 ```
 
 ###~~初始化数据库~~
-~~在mysql中创建一个叫`stuinfo`的数据库，然后执行`./create_table.py`~~  
-如需删除所有数据重新创建，执行`./create_table.py --drop`
+~~在mysql中创建一个叫`stuinfo`的数据库，然后执行`./create_table.py localhost`~~  
+如需删除所有数据重新创建，执行`./create_table.py localhost --drop`  
+如果在docker中执行，把 localhost 替换成 db 即可
 
 ###启动测试服务器
 运行`./start_debug_server.py`
--- create_table.py
+++ create_table.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from sys import argv
+from sys import argv, exit
+from stuinfo import app
 from stuinfo.db import create_table
 
 
 if __name__ == '__main__':
-    drop = len(argv) > 1 and argv[1] == '--drop'
+    if len(argv) <= 1:
+        print('Need db host')
+        exit()
+    app.config['MYSQL_HOST'] = argv[1]
+    drop = len(argv) > 2 and argv[2] == '--drop'
     print('drop:', drop)
     create_table(drop)
